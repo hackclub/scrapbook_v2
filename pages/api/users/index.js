430c195f10071bef72d6bc3aadecd12c44cb8047
Jessica Card <jessicard@mac.com>
2022-04-24 21:00:06 -0400

    Clear repository

--- pages/api/users/index.js
+++
@@ -1,6 +0,0 @@
-import prisma from '../../../lib/prisma'
-
-export const getRawUsers = (onlyFull = false) =>
-  prisma.accounts.findMany(onlyFull ? { where: { fullSlackMember: true } } : {})
-
-export default async (req, res) => getRawUsers().then(u => res.json(u || []))
