0974a8a4d9a087c53c6ee7144c3298df1c315b8b
Jessica Card <jessicard@mac.com>
2022-04-27 15:07:32 -0400

    Add posts on user page

--- web/src/components/User/User/User.js
+++ web/src/components/User/User/User.js
@@ -1,9 +1,12 @@
 import { Link, routes, navigate } from '@redwoodjs/router'
 
+import PostsCell from 'src/components/Post/PostsCell'
+
 const User = ({ user }) => {
   return (
     <>
       <h2>{user.email}</h2>
+      <PostsCell />
     </>
   )
 }
