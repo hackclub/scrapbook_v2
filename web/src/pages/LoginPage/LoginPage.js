97204730f1567ec1382e3df7ae0b25b67516eca5
Jessica Card <jessicard@mac.com>
2022-04-27 20:47:52 -0400

    Add in login errors

--- web/src/pages/LoginPage/LoginPage.js
+++ web/src/pages/LoginPage/LoginPage.js
@@ -6,6 +6,7 @@ import {
   TextField,
   PasswordField,
   Submit,
+  FieldError,
 } from '@redwoodjs/forms'
 import { useAuth } from '@redwoodjs/auth'
 import { MetaTags } from '@redwoodjs/web'
@@ -26,43 +27,63 @@ const LoginPage = () => {
   }, [])
 
   const onSubmit = async (data) => {
-    await logIn({ ...data })
+    const response = await logIn({ ...data })
+
+    if (response.message) {
+      console.log(response.message)
+    } else if (response.error) {
+      console.log(response.error)
+    }
   }
 
   return (
     <>
       <MetaTags title="Login" />
+
+      <header>
+        <h2>Login</h2>
+      </header>
+
       <div>
-        <div>
-          <header>
-            <h2>Login</h2>
-          </header>
+        <Form onSubmit={onSubmit}>
+          <Label name="username">Email</Label>
+          <TextField
+            name="username"
+            ref={usernameRef}
+            validation={{
+              required: {
+                value: true,
+                message: "Email is required",
+              },
+            }}
+          />
+
+          <FieldError name="username" />
+
+          <Label name="password">Password</Label>
+          <PasswordField
+            name="password"
+            validation={{
+              required: {
+                value: true,
+                message: "Password is required",
+              },
+            }}
+          />
+
+          <FieldError name="password" />
 
           <div>
-            <Form onSubmit={onSubmit}>
-              <Label>Email</Label>
-              <TextField name="username" ref={usernameRef} />
-
-              <Label>Password</Label>
-              <PasswordField name="password" />
-
-              <div>
-                <Link to={routes.forgotPassword()}>
-                  Forgot Password?
-                </Link>
-              </div>
-              <div>
-                <Submit>Login</Submit>
-              </div>
-            </Form>
+            <Link to={routes.forgotPassword()}>Forgot Password?</Link>
           </div>
-        </div>
-        <div>
-          <span>Don't have an account?</span>
-          <Link to={routes.signup()}>
-            Sign up!
-          </Link>
-        </div>
+
+          <Submit>Login</Submit>
+        </Form>
+      </div>
+
+      <div>
+        <span>Don't have an account?</span>{' '}
+        <Link to={routes.signup()}>Sign up!</Link>
       </div>
     </>
   )
